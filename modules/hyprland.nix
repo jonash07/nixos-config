fc59b09a0a4a17515108eb92eec75a37fc6a6269
jonash07 <jonash07f@gmail.com>
2025-06-17 15:45:30 +0200

    do i even need this

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,16 +4,6 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    withUWSM = true;
-  };
-
-  programs.uwsm = {
-    enable = true;
-    waylandCompositors.hyprland = {
-      prettyName = "Hyprland";
-      comment = "Hyprland compositor managed by UWSM";
-      binPath = "/run/current-system/sw/bin/Hyprland";
-    };
   };
 
   environment.systemPackages = with pkgs; [
