756cecbd1cfb2e99b9082bc4a8d059813e9b373b
jonash07 <jonash07f@gmail.com>
2025-07-30 21:35:53 +0200

    many things

--- modules/packages.nix
+++ modules/packages.nix
@@ -35,5 +35,6 @@
     pavucontrol
     mpv
     mtpfs
+    playerctl
   ];
 }
--- modules/session_variables.nix
+++ modules/session_variables.nix
@@ -5,6 +5,11 @@
     EDITOR = "nvim";
   };
 
+  environment.interactiveShellInit = ''
+    alias nrs="sudo nixos-rebuild switch --flake /home/jim/nixos-config#default"
+    alias nfu="sudo nix flake update --flake /home/jim/nixos-config#default"
+  '';
+
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     # NIXOS_OZONE_WL = "1";
