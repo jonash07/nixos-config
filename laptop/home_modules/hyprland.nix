b36237755a32ddf09a410dac4d77c2198bb7433f
jonash07 <jonash07@proton.me>
2025-09-11 21:41:48 +0200

    laptop scale

--- laptop/home_modules/hyprland.nix
+++ laptop/home_modules/hyprland.nix
@@ -9,7 +9,7 @@
 
       # See https://wiki.hyprland.org/Configuring/Monitors/
       monitor = [
-        "DP-1, 1920x1080@60, 0x0, 1"
+        "DP-1, 1920x1080@60, 0x0, 0.5"
       ];
     };
   };
