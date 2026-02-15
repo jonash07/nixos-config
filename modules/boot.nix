fa2af85cd2b57680fb8ade6965da98db87a4c91a
jonash07 <jonash07@proton.me>
2026-02-15 16:47:40 +0100

    latest kernel

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 1;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   boot.kernelParams = [ "quiet" ];
 
   systemd.services.NetworkManager-wait-online.enable = false;
 }
+
