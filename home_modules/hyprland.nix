fdba387c24742c6fd929c5cd63b46b01f56156f4
jonash07 <jonash07f@gmail.com>
2026-03-13 17:23:43 +0100

    separates style from nix configured hyprland

--- home_modules/hyprland.nix
+++ home_modules/hyprland.nix
@@ -33,8 +33,8 @@
       # Set programs that you use
       "$terminal" = "alacritty";
       "$fileManager" = "dolphin";
-      "$menu" = "rofi -show drun -drun-display-format {name}";
       "$screenLock" = "hyprlock";
+      # "$menu" = "rofi -show drun -drun-display-format {name}";
 
       #################
       ### AUTOSTART ###
@@ -66,84 +66,84 @@
       # Refer to https://wiki.hyprland.org/Configuring/Variables/
       
       # https://wiki.hyprland.org/Configuring/Variables/#general
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
+      # general = {
+      #   gaps_in = 5;
+      #   gaps_out = 10;
     
-        border_size = 0;
+      #   border_size = 0;
     
         # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
+      #   "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      #   "col.inactive_border" = "rgba(595959aa)";
     
         # Set to true enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false;
+      #   resize_on_border = false;
     
         # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-        allow_tearing = false;
+      #   allow_tearing = false;
     
-        layout = "dwindle";
-      };
+      #   layout = "dwindle";
+      # };
       
       # https://wiki.hyprland.org/Configuring/Variables/#decoration
-      decoration = {
-        rounding = 4;
-        rounding_power = 2;
+      # decoration = {
+      #   rounding = 4;
+      #   rounding_power = 2;
     
         # Change transparency of focused and unfocused windows
-        active_opacity = 1.0;
-        inactive_opacity = 1.0;
+      #   active_opacity = 1.0;
+      #   inactive_opacity = 1.0;
     
-        shadow = {
-          enabled = true;
-          range = 4;
-          render_power = 3;
-          color = "rgba(1a1a1aee)";
-        };
+      #   shadow = {
+      #     enabled = true;
+      #     range = 4;
+      #     render_power = 3;
+      #     color = "rgba(1a1a1aee)";
+      #   };
     
         # https://wiki.hyprland.org/Configuring/Variables/#blur
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
+      #   blur = {
+      #     enabled = true;
+      #     size = 3;
+      #     passes = 1;
   
-          vibrancy = 0.1696;
-        };
-      };
+      #     vibrancy = 0.1696;
+      #   };
+      # };
       
       # https://wiki.hyprland.org/Configuring/Variables/#animations
-      animations = {
-        enabled = true;
+      # animations = {
+      #   enabled = true;
     
         # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
     
-        bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear, 0, 0, 1, 1"
-          "almostLinear, 0.5,0.5,0.75,1.0"
-          "quick, 0.15,0,0.1,1"
-        ];
+      #   bezier = [
+      #     "easeOutQuint,0.23,1,0.32,1"
+      #     "easeInOutCubic,0.65,0.05,0.36,1"
+      #     "linear, 0, 0, 1, 1"
+      #     "almostLinear, 0.5,0.5,0.75,1.0"
+      #     "quick, 0.15,0,0.1,1"
+      #   ];
     
-        animation = [
-          "global, 1, 10, default"
-          "border, 1, 5.39, easeOutQuint"
-          "windows, 1, 4.79, easeOutQuint"
-          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-          "windowsOut, 1, 1.49, linear, popin 87%"
-          "fadeIn, 1, 1.73, almostLinear"
-          "fadeOut, 1, 1.46, almostLinear"
-          "fade, 1, 3.03, quick"
-          "layers, 1, 3.81, easeOutQuint"
-          "layersIn, 1, 4, easeOutQuint, fade"
-          "layersOut, 1, 1.5, linear, fade"
-          "fadeLayersIn, 1, 1.79, almostLinear"
-          "fadeLayersOut, 1, 1.39, almostLinear"
-          "workspaces, 1, 1.94, almostLinear, fade"
-          "workspacesIn, 1, 1.21, almostLinear, fade"
-          "workspacesOut, 1, 1.94, almostLinear, fade"
-        ];
-      };
+      #   animation = [
+      #     "global, 1, 10, default"
+      #     "border, 1, 5.39, easeOutQuint"
+      #     "windows, 1, 4.79, easeOutQuint"
+      #     "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+      #     "windowsOut, 1, 1.49, linear, popin 87%"
+      #     "fadeIn, 1, 1.73, almostLinear"
+      #     "fadeOut, 1, 1.46, almostLinear"
+      #     "fade, 1, 3.03, quick"
+      #     "layers, 1, 3.81, easeOutQuint"
+      #     "layersIn, 1, 4, easeOutQuint, fade"
+      #     "layersOut, 1, 1.5, linear, fade"
+      #     "fadeLayersIn, 1, 1.79, almostLinear"
+      #     "fadeLayersOut, 1, 1.39, almostLinear"
+      #     "workspaces, 1, 1.94, almostLinear, fade"
+      #     "workspacesIn, 1, 1.21, almostLinear, fade"
+      #     "workspacesOut, 1, 1.94, almostLinear, fade"
+      #   ];
+      # };
       
       # Ref https://wiki.hyprland.org/Configuring/Workspace-Rules/
       # "Smart gaps" / "No gaps when only"
@@ -292,6 +292,10 @@
         "suppress_event maximize, match:class .*"
         # Fix some dragging issues with XWayland
         "no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float true, match:fullscreen false, match:pin false" 
+      ];
+
+      source = [
+        "./hyprland_style.conf"
       ];
 
     };
