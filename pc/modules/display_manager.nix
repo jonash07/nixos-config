dbad22870bbb329c4aa97b9a4ecf07bb1db86ba4
jonash07 <jonash07f@gmail.com>
2026-04-02 15:35:33 +0200

    themed sddm cause fuck canonical and lightdm

--- pc/modules/display_manager.nix
+++ pc/modules/display_manager.nix
@@ -1,10 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  services.xserver.displayManager.lightdm = {
-    enable = true;
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      theme = "catppuccin-mocha-mauve";
+
+      wayland = {
+        enable = true;
+    
+      };
+
+    };
 
   };
+
+  environment.systemPackages = [
+  (pkgs.catppuccin-sddm.override {
+    flavor = "mocha";
+    accent = "mauve";
+    font = "Mononoki Nerd Font";
+    fontSize = "16";
+   })
+  ];
 
 }
 
