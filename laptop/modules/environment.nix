889196b3938cfe9eabafcf665eebde7421920554
jonash07 <jonash07@proton.me>
2026-04-03 17:08:15 +0200

    sudo not needed

--- laptop/modules/environment.nix
+++ laptop/modules/environment.nix
@@ -5,7 +5,7 @@
     nrs = "sudo nixos-rebuild switch --flake ~/nixos-config/laptop#laptop";
     nrb = "sudo nixos-rebuild boot --flake ~/nixos-config/laptop#laptop";
     nrt = "sudo nixos-rebuild test --flake ~/nixos-config/laptop#laptop";
-    nfu = "sudo nix flake update --flake ~/nixos-config/laptop";
+    nfu = "nix flake update --flake ~/nixos-config/laptop";
 
   };
 
--- pc/modules/environment.nix
+++ pc/modules/environment.nix
@@ -5,7 +5,7 @@
     nrs = "sudo nixos-rebuild switch --flake ~/nixos-config/pc#pc";
     nrb = "sudo nixos-rebuild boot --flake ~/nixos-config/pc#pc";
     nrt = "sudo nixos-rebuild test --flake ~/nixos-config/pc#pc";
-    nfu = "sudo nix flake update --flake ~/nixos-config/pc";
+    nfu = "nix flake update --flake ~/nixos-config/pc";
 
   };
 
