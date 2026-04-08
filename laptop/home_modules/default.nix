041aea750e19531be2d975103ecf6e706cbcb5dd
jonash07 <jonash07@proton.me>
2026-04-08 14:13:26 +0200

    pre hyprland config fuckery

--- laptop/home_modules/default.nix
+++ laptop/home_modules/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./files.nix
     ./hyprland.nix
     ./services.nix
 
---
+++ laptop/home_modules/files.nix
@@ -0,0 +1,32 @@
+{ ... }:
+
+{
+  home.file.".config/.mischypr/misc.conf" = {
+    enable = true;
+    text = ''
+device {
+  name=epic-mouse-v1
+  sensitivity=-0.500000
+}
+
+input {
+  touchpad {
+    disable_while_typing=false
+    natural_scroll=false
+  }
+  follow_mouse=1
+  kb_layout=pl
+  kb_model=
+  kb_options=
+  kb_rules=
+  kb_variant=
+  sensitivity=0
+}
+
+monitor=eDP-1, 1920x1080@60, 0x0, 1
+    '';
+  
+  };
+
+}
+
